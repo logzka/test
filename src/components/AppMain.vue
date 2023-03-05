72720c4c6cbae7e5ac355a6b2b92decac8193405
Anastasiia Davydova <anastasiia.d@interactivated.me>
2023-03-05 10:33:05 +0200

    set element ui plus.

---
+++ src/components/AppMain.vue
@@ -0,0 +1,29 @@
+<template>
+  <el-container>
+    <el-header>
+      User things
+    </el-header>
+
+    <el-main>
+      <el-button
+        bg
+        text
+        type="success"
+        >text</el-button
+      >
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: 'AppMain',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
